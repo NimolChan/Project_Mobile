bf8599236a7b36d1dba14cd4f57c397423edea7d
kimhorn20223 <155133888+kimhorn20223@users.noreply.github.com>
2025-06-12 04:48:24 +0700

    Collection

--- lib/screens/collection/index.dart
+++ lib/screens/collection/index.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:stylishcloset/screens/collection/widget/cart_collection.dart';
 import 'package:stylishcloset/widgets/common/navigation.dart';
 
 class CollectionScreen extends StatelessWidget {
@@ -7,8 +8,38 @@ class CollectionScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Collection')),
-      body: const Center(child: Text('Collection Content')),
+      appBar: AppBar(
+        title: const Text(
+          'Collection',
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.white,
+        leading: IconButton(icon: Image.asset( "assets/images/logo.png", width:50, height:50),
+          onPressed: () {
+          // Navigator.push(context, MaterialPageRoute(builder: (context) => const NavigationScreen()));
+        }), 
+        // actions: [
+        //   Padding(
+        //     padding: const EdgeInsets.only(right: 16.0),
+        //     child: Image.asset("assets/images/logo.png", width: 40, height: 40),
+        //   )
+        // ],
+        ),
+      body:
+
+        Container(
+          padding: const EdgeInsets.symmetric(horizontal: 10.0),
+          child: Column(
+           children: [
+            CartCollection(title: "Wedding", image: "assets/images/logo.png", color: "0xFF847B7B"),
+           ],
+          ),
+        ),
       bottomNavigationBar: const CustomBottomNav(),
     );
   }
